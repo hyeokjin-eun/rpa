5ee0c7b1f4849c707995edfa3f4daf493fa922ea
hyeokjin-eun <gurwls2399@gmail.com>
2024-02-19 01:25:34 +0900

    feat : data command 수정

--- selenium/src/main/java/com/rpa/selenium/api/data/dto/request/impl/sub/foreach/impl/DataCommand.java
+++ selenium/src/main/java/com/rpa/selenium/api/data/dto/request/impl/sub/foreach/impl/DataCommand.java
@@ -2,13 +2,12 @@ package com.rpa.selenium.api.data.dto.request.impl.sub.foreach.impl;
 
 import com.rpa.selenium.api.data.dto.request.impl.sub.foreach.ForEachCommand;
 import com.rpa.selenium.selenium.WebElement;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Builder
@@ -18,20 +17,32 @@ import java.util.Map;
 @Slf4j
 public class DataCommand implements ForEachCommand {
 
-    private String key;
-
-    private String target;
-
-    private String description;
+    private List<Field> fields = new ArrayList<>();
 
     @Override
     public boolean logic(WebElement webElement) {
         Map<String, Map<String, String>> temp = new HashMap<>();
-        Map<String, String> data = new HashMap<>();
-        data.put("value", webElement.getText(this.target));
-        data.put("description", this.description);
-        temp.put(this.key, data);
+        for (Field field : fields) {
+            Map<String, String> data = new HashMap<>();
+            data.put("value", webElement.getText(field.getTarget()));
+            data.put("description", field.getDescription());
+            temp.put(field.getKey(), data);
+        }
+
         log.info("Data Save : " + temp.toString());
         return true;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Field {
+        private String key;
+
+        private String target;
+
+        private String description;
     }
 }
